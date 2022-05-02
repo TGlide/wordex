b161632980adee9a39d94fdafa359ccfd94a1554
tglide <26071571+TGlide@users.noreply.github.com>
2022-05-02 20:15:21 +0100

    feat: add theme switch

---
+++ src/lib/utils/theme.ts
@@ -0,0 +1,6 @@
+export function switchTheme() {
+	const oldValue = document.documentElement.getAttribute('data-theme');
+	const newValue = oldValue === 'dark' ? 'light' : 'dark';
+	document.documentElement.setAttribute('data-theme', newValue);
+	localStorage.setItem('theme', newValue);
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,38 @@
+<script>
+	import { switchTheme } from '$lib/utils/theme';
+</script>
+
 <section class="container">
 	<h1>Index page</h1>
+	<button on:click={switchTheme}>Switch theme</button>
 </section>
 
 <style>
+	section {
+		--btn-bg: var(--palette-grey-90);
+		--btn-bg-hover: var(--palette-grey-80);
+	}
+
+	:global([data-theme='dark']) section {
+		--btn-bg: var(--palette-grey-20);
+		--btn-bg-hover: var(--palette-grey-30);
+	}
+
 	h1 {
 		font-family: var(--ff-display);
 		font-size: 3rem;
+	}
+
+	button {
+		background-color: var(--btn-bg);
+		border-radius: var(--radii-sm);
+
+		padding: 0.75rem 1rem;
+		margin-top: 1rem;
+	}
+
+	button:hover {
+		cursor: pointer;
+		background-color: var(--btn-bg-hover);
 	}
 </style>
