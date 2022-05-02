8a253fed8c1b34b2e765893eb127ef50d414b9cc
tglide <26071571+TGlide@users.noreply.github.com>
2022-05-02 19:49:31 +0100

    chore: setup base styles

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import '../styles/index.css';
+</script>
+
+<main>
+	<slot />
+</main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="container">
+	<h1>Index page</h1>
+</section>
+
+<style>
+	h1 {
+		font-family: var(--ff-display);
+		font-size: 3rem;
+	}
+</style>
